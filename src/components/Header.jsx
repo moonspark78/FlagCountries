8b8c1f8fdc3928dd8075bfb6068a6144222665df
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-04 19:25:31 +0100

    :rocket: Navbar  :tada:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './Header.css';
 import logo from "../logo.jpeg"
+import search from "../search-w.png"
 
 
 export const Header = () => {
   return (
     <div className='navbar'>
-      <img alt='logo' src={logo} className='logo'/>
+      <img alt='logo' style={{height: "80px"}} src={logo} className='logo'/>
 
       <div className='serach-box'>
         <input placeholder='Search ...' type=''/>
-        <img src='' alt='search-icon'/>
+        <img src={search} alt='search-icon'/>
       </div>
 
-      <img src='' alt='toggle-icon' className='toggle-icon'/>
     </div>
   )
 }
