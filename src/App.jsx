240758a02926ae266e34db4890f284624f281df3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-04 16:48:35 +0100

    initialise the fetch to the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import logo from "./logo.svg"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState([])
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    const fetchData =  async () => {
+      try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        const data = await response.json();
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  },[]);
 
   
   return (
